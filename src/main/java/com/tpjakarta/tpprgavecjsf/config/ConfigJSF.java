e7a5dbd445bd5971b275cd7d2eb462f2ac7564d0
Steve <ramiandrisoasteve@gmail.com>
2024-04-07 10:16:38 +0300

    exercie 1 avec amelioration

---
+++ src/main/java/com/tpjakarta/tpprgavecjsf/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.tpjakarta.tpprgavecjsf.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Steve
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
+
