9c73a9cba340a667b8f2e16c02d7d516d4b7f022
Steve <ramiandrisoasteve@gmail.com>
2024-04-09 03:52:39 +0300

    TP 3 test exercice 1

--- src/main/java/com/tpjakarta/tpprgavecjsf/jsf/Bean1.java
+++ src/main/java/com/tpjakarta/tpprgavecjsf/jsf/Bean1.java
@@ -33,6 +33,6 @@ public class Bean1 {
     }
 
     public String afficher() {
-        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+        return "affichage__1?nb=" + nombre + "&amp;faces-redirect=true";
     }
 }
